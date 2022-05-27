af99eb7e8c1ea58b9eda5fb4cb6635f6f96289f7
肖冰君 <bingjunxiao@emotibot.com>
2022-05-27 20:50:31 +0800

    fix: gen-yaml

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,6 @@ import app from './modules/app'
 import settings from './modules/settings'
 import user from './modules/user'
 import publishapp from './modules/publishapp'
-import applist from './modules/applist'
 
 Vue.use(Vuex)
 
@@ -14,8 +13,7 @@ const store = new Vuex.Store({
     app,
     settings,
     user,
-    publishapp,
-    applist
+    publishapp
   },
   getters
 })
