36f5e60db8bbbcadf8d9821e6d8770998973deac
肖冰君 <bingjunxiao@emotibot.com>
2022-06-15 12:23:58 +0800

    fix: linemarker

--- src/views/dashboard/LineMarker.vue
+++ src/views/dashboard/LineMarker.vue
@@ -38,7 +38,6 @@ export default {
     return {
       chart: null,
       xAxisData: [],
-      yAxisItem: [],
       yAxisData: {},
       series: [],
       rgba: {},
@@ -63,7 +62,7 @@ export default {
   watch: {
     trafficMetric: {
       handler: function(val, oldVal) {
-        if (val) {
+        if (val[0]) {
           this.xAxisData.push(parseTime(val[0].timestamp, '{h}:{i}:{s}'))
           Object.keys(val[0].value).forEach(element => {
             if (typeof (this.yAxisData[element]) !== 'object') {
@@ -72,8 +71,8 @@ export default {
               this.count++
             }
             this.yAxisData[element].push(val[0].value[element])
-            console.log(element)
-            console.log(this.yAxisData[element])
+            // console.log(element)
+            // console.log(this.yAxisData[element])
           })
           this.setOption(this.xAxisData, this.yAxisData)
           if (this.xAxisData.length === 5) {
@@ -99,114 +98,119 @@ export default {
   methods: {
     initChart() {
       this.chart = echarts.init(document.getElementById(this.id))
-      this.setOption(this.xAxisData, this.yAxisData)
+      this.rgba['loading'] = this.rgbas[this.count]
+      this.setOption([0], { loading: 0 })
     },
     setOption(xAxisData, yAxisData) {
-      Object.keys(yAxisData).forEach(element => {
-        console.log(element)
-        console.log(yAxisData[element])
-        const series = {
-          name: element,
-          type: 'line',
-          smooth: true,
-          symbol: 'circle',
-          symbolSize: 5,
-          showSymbol: false,
-          lineStyle: {
-            width: 1
-          },
-          areaStyle: {
-            color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
-              offset: 0,
-              color: this.rgba[element][0]
-            }, {
-              offset: 0.8,
-              color: this.rgba[element][1]
-            }], false),
-            shadowColor: 'rgba(0, 0, 0, 0.1)',
-            shadowBlur: 10
-          },
-          itemStyle: {
-            color: this.rgba[element][2],
-            borderColor: this.rgba[element][3],
-            borderWidth: 12
-          },
-          data: yAxisData[element]
-        }
-        this.series.push(series)
-        this.chart.setOption({
-          backgroundColor: '#394056',
-          title: {
-            top: 20,
-            text: this.id,
-            textStyle: {
-              fontWeight: 'normal',
-              fontSize: 16,
-              color: '#F1F1F3'
+      if (this.series.length < Object.keys(yAxisData).length) {
+        Object.keys(yAxisData).forEach(element => {
+        // console.log(element)
+        // console.log(yAxisData[element])
+          const series = {
+            name: element,
+            type: 'line',
+            smooth: true,
+            symbol: 'circle',
+            symbolSize: 5,
+            showSymbol: false,
+            lineStyle: {
+              width: 1
             },
-            left: '1%'
-          },
-          tooltip: {
-            trigger: 'axis',
-            axisPointer: {
-              lineStyle: {
-                color: '#57617B'
-              }
-            }
-          },
-          legend: {
-            top: 20,
-            icon: 'rect',
-            itemWidth: 14,
-            itemHeight: 5,
-            itemGap: 13,
-            data: Object.keys(yAxisData),
-            right: '4%',
-            textStyle: {
-              fontSize: 12,
-              color: '#F1F1F3'
-            }
-          },
-          grid: {
-            top: 100,
-            left: '2%',
-            right: '2%',
-            bottom: '2%',
-            containLabel: true
-          },
-          xAxis: [{
-            type: 'category',
-            boundaryGap: false,
-            axisLine: {
-              lineStyle: {
-                color: '#57617B'
-              }
+            areaStyle: {
+              color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+                offset: 0,
+                color: this.rgba[element][0]
+              }, {
+                offset: 0.8,
+                color: this.rgba[element][1]
+              }], false),
+              shadowColor: 'rgba(0, 0, 0, 0.1)',
+              shadowBlur: 10
             },
-            data: xAxisData
-          }],
-          yAxis: [{
-            type: 'value',
-            name: '(KB)',
-            axisTick: {
-              show: false
+            itemStyle: {
+              color: this.rgba[element][2],
+              borderColor: this.rgba[element][3],
+              borderWidth: 12
             },
-            axisLine: {
-              lineStyle: {
-                color: '#57617B'
-              }
-            },
-            axisLabel: {
-              margin: 10,
-              fontSize: 14
-            },
-            splitLine: {
-              lineStyle: {
-                color: '#57617B'
-              }
-            }
-          }],
-          series: this.series
+            data: yAxisData[element]
+          }
+          this.series.push(series)
         })
+      }
+
+      // console.log(this.series)
+      this.chart.setOption({
+        backgroundColor: '#394056',
+        title: {
+          top: 20,
+          text: this.id,
+          textStyle: {
+            fontWeight: 'normal',
+            fontSize: 16,
+            color: '#F1F1F3'
+          },
+          left: '1%'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            lineStyle: {
+              color: '#57617B'
+            }
+          }
+        },
+        legend: {
+          top: 20,
+          icon: 'rect',
+          itemWidth: 14,
+          itemHeight: 5,
+          itemGap: 13,
+          data: Object.keys(yAxisData),
+          right: '4%',
+          textStyle: {
+            fontSize: 12,
+            color: '#F1F1F3'
+          }
+        },
+        grid: {
+          top: 100,
+          left: '2%',
+          right: '2%',
+          bottom: '2%',
+          containLabel: true
+        },
+        xAxis: [{
+          type: 'category',
+          boundaryGap: false,
+          axisLine: {
+            lineStyle: {
+              color: '#57617B'
+            }
+          },
+          data: xAxisData
+        }],
+        yAxis: [{
+          type: 'value',
+          name: '(KB)',
+          axisTick: {
+            show: false
+          },
+          axisLine: {
+            lineStyle: {
+              color: '#57617B'
+            }
+          },
+          axisLabel: {
+            margin: 10,
+            fontSize: 14
+          },
+          splitLine: {
+            lineStyle: {
+              color: '#57617B'
+            }
+          }
+        }],
+        series: this.series
       })
     }
   }
